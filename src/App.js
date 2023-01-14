c52649e5bb8faa992397095e9cc23e3fdb521b75
a6uxa4 <a6uxa4@icloud.com>
2023-01-15 00:20:29 +0600

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
+
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [state, setState] = useState([])
+	const handleEventClick = (clickInfo) => {
+		if (
+			alert(
+				`Are you sure you want to delete the event '${clickInfo.event.title}'`,
+			)
+		) {
+			clickInfo.event.remove()
+		}
+	}
+	function handleDateSelect(selectInfo) {
+		let color = prompt('Color please')
+		let title = prompt('Please enter a new title for your event')
+		let calendarApi = selectInfo.view.calendar
+
+		calendarApi.unselect()
+
+		if (color) {
+			setState(
+				calendarApi.addEvent({
+					id: Math.random().toString(),
+					title,
+					color,
+					start: selectInfo.startStr,
+					end: selectInfo.endStr,
+				}),
+			)
+		}
+	}
+	return (
+		<FullCalendar
+			plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+			headerToolbar={{
+				left: 'prevYear,prev,dayGridMonth',
+				center: 'title',
+				right: 'timeGridDay,next,nextYear',
+			}}
+			initialView='dayGridMonth'
+			eventBackgroundColor={state.color}
+			eventBorderColor='black'
+			locales='ru'
+			firstDay='1'
+			height={'100vh'}
+			editable={true}
+			selectable={true}
+			selectMirror={true}
+			dayMaxEvents={true}
+			weekends={true}
+			buttonText={{ day: 'День', month: 'Месяц' }}
+			select={(w) => handleDateSelect(w)}
+			eventClick={(w) => handleEventClick(w)}
+			events={state}
+		/>
+	)
 }
 
-export default App;
+export default App
